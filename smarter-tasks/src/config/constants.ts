1a3a170d8b19e30d93bac7c2270b55ae132864de
Shylesh <shylesharepelly@gmail.com>
2023-06-28 17:52:47 +0530

    converted to vite

--- smarter-tasks/src/config/constants.ts
+++ smarter-tasks/src/config/constants.ts
@@ -1,2 +1,2 @@
 
-export const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT
+export const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT
---
+++ smarter-tasks/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+
+
+export default ({ mode }) => {
+  return defineConfig({
+    build: {
+        outDir: 'build',
+      },
+    plugins: [react()],
+    define: {
+      "process.env.NODE_ENV": `"${mode}"`,
+    },
+  });
+};
