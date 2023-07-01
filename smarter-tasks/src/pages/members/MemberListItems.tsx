7e038d78334e826ddaa2bb3b70e2c6231ffbfc00
Shylesh <shylesharepelly@gmail.com>
2023-07-01 19:30:16 +0530

    changes

--- smarter-tasks/src/pages/members/MemberListItems.tsx
+++ smarter-tasks/src/pages/members/MemberListItems.tsx
@@ -30,7 +30,7 @@ export default function MemberListItems() {
   return (
     <>
       {members.map((member: any) => (
-        <div key={member.id} className="block member p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <div key={member.id} className="member p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
           
           <h5 className="mb-2 text-xl font-medium tracking-tight text-gray-900 dark:text-white">{member.name}</h5>
           <button onClick={()=> { deleteHandler(member.id) }}>
